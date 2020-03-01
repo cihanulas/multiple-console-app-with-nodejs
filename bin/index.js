b05316326fa5f9a1c2c54c714f72ccca254adabc
Cihan Ulas <cihanulas@yahoo.com>
2020-03-01 20:25:58 +0300

    add second arg for search joke.

--- bin/index.js
+++ bin/index.js
@@ -6,16 +6,32 @@ const axios = require("axios");
 const options = yargs
  .usage("Usage: -n <name>")
  .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .option("s", { alias: "search", describe: "Search term", type: "string" })
  .argv;
 
 const greeting = `Hello, ${options.name}!`;
 console.log(greeting);
 
-console.log("Here's a random joke for you:");
+if (options.search) {
+ console.log(`Searching for jokes about ${options.search}...`)
+} else {
+ console.log("Here's a random joke for you:");
+}
 
-const url = "https://icanhazdadjoke.com/";
+// The url depends on searching or not
+const url = options.search ? `https://icanhazdadjoke.com/search?term=${escape(options.search)}` : "https://icanhazdadjoke.com/";
 
 axios.get(url, { headers: { Accept: "application/json" } })
  .then(res => {
-   console.log(res.data.joke);
+   if (options.search) {
+     // if searching for jokes, loop over the results
+     res.data.results.forEach( j => {
+       console.log("\n" + j.joke);
+     });
+     if (res.data.results.length === 0) {
+       console.log("no jokes found :'(");
+     }
+   } else {
+     console.log(res.data.joke);
+   }
  });
