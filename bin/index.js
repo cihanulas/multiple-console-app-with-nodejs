d1eb44037e08b01794fb0bd16d9ca9eb35a35211
Cihan Ulas <cihanulas@yahoo.com>
2020-03-01 17:46:55 +0300

    update index.js for a nice console output

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
-// The first line that begins with #! is usually called a �shebang.� 
-// This is normally only used on Linux or UNIX operating systems to inform the system what type of script is included in the rest of the text file. 
-// However, this first line is also required for Node.js scripts to be installed and run properly on macOS and Windows.
+const chalk = require("chalk");
+const boxen = require("boxen");
 
-console.log( "Hello!" );
+const greeting = chalk.white.bold("Hello!");
 
+const boxenOptions = {
+ padding: 1,
+ margin: 1,
+ borderStyle: "round",
+ borderColor: "green",
+ backgroundColor: "#555555"
+};
+const msgBox = boxen( greeting, boxenOptions );
 
-
+console.log(msgBox);
