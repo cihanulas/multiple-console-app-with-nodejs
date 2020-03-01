45c20bcdff11e469338fdcc33b69d462fb95e09d
Cihan Ulas <cihanulas@yahoo.com>
2020-03-01 17:42:02 +0300

    install application in global scope.

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// The first line that begins with #! is usually called a “shebang.” 
+// The first line that begins with #! is usually called a ï¿½shebang.ï¿½ 
 // This is normally only used on Linux or UNIX operating systems to inform the system what type of script is included in the rest of the text file. 
 // However, this first line is also required for Node.js scripts to be installed and run properly on macOS and Windows.
 
--- readme.md
+++ readme.md
@@ -1,2 +1,20 @@
 test the application. 
 node .
+
+make the application global 
+npm install -g .
+
+Now you can run the application from everywhere by just calling 
+
+> hello
+
+all the applications under bin will be executed 
+"bin": {
+    "hello": "./bin/index.js"
+  }
+
+to uninstall this 
+npm uninstall -g hello-cli
+
+Tip: You can list all globally installed Node.js modules using npm ls -g --depth=0.
+npm ls -g --depth=0
